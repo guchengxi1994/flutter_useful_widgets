e29c4740103055974fc476e829dfed62c04255bd
壹零 <guchengxi1994@qq.com>
2023-05-31 15:54:54 +0800

    Update dates_range_picker.dart

--- lib/date_pickers/dates_range_picker.dart
+++ lib/date_pickers/dates_range_picker.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_useful_widgets/extensions/extensions.dart';
 import 'package:just_the_tooltip/just_the_tooltip.dart';
 
+typedef OnDatePicked = void Function(List<DateTime?> dates);
+
 class UsefulDateRangePicker extends StatefulWidget {
   const UsefulDateRangePicker(
       {Key? key,
@@ -11,7 +13,10 @@ class UsefulDateRangePicker extends StatefulWidget {
       this.firstDate,
       this.lastDate,
       this.locale = "zh",
-      this.textStyle})
+      this.textStyle,
+      this.multiRow = false,
+      this.decoration,
+      this.onDatePicked})
       : super(key: key);
   final double calendarWidth;
   final double calendarHeight;
@@ -19,6 +24,9 @@ class UsefulDateRangePicker extends StatefulWidget {
   final DateTime? lastDate;
   final String locale;
   final TextStyle? textStyle;
+  final bool multiRow;
+  final BoxDecoration? decoration;
+  final OnDatePicked? onDatePicked;
 
   @override
   State<UsefulDateRangePicker> createState() => UsefulDateRangePickerState();
@@ -62,53 +70,79 @@ class UsefulDateRangePickerState extends State<UsefulDateRangePicker> {
                     lastDate = value[2]!;
                   });
                 }
+
+                if (widget.onDatePicked != null) {
+                  widget.onDatePicked!([firstDate, lastDate]);
+                }
               }
             },
             initialValue: [firstDate, lastDate],
           ),
         ),
-        child: SizedBox(
-          width: 300,
-          height: 30,
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              _dateWrapper(firstDate.auto(locale: widget.locale)),
-              widget.locale == "zh"
-                  ? Text(
-                      "至",
-                      style: textStyle,
-                    )
-                  : Text(
-                      "To",
-                      style: textStyle,
-                    ),
-              _dateWrapper(lastDate.auto(locale: widget.locale))
-            ],
-          ),
-        ));
+        child: !widget.multiRow
+            ? SizedBox(
+                width: 350,
+                height: 30,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    _dateWrapper(firstDate.auto(locale: widget.locale)),
+                    widget.locale == "zh"
+                        ? Text(
+                            "至",
+                            style: textStyle,
+                          )
+                        : Text(
+                            "To",
+                            style: textStyle,
+                          ),
+                    _dateWrapper(lastDate.auto(locale: widget.locale))
+                  ],
+                ),
+              )
+            : SizedBox(
+                height: 85,
+                width: 200,
+                child: Column(
+                  children: [
+                    _dateWrapper(firstDate.auto(locale: widget.locale)),
+                    widget.locale == "zh"
+                        ? Text(
+                            "至",
+                            style: textStyle,
+                          )
+                        : Text(
+                            "To",
+                            style: textStyle,
+                          ),
+                    _dateWrapper(lastDate.auto(locale: widget.locale))
+                  ],
+                ),
+              ));
   }
 
   Widget _dateWrapper(String date) {
     return Container(
       padding: const EdgeInsets.all(5),
-      width: 120,
+      width: 150,
       height: 30,
-      decoration: BoxDecoration(
-          borderRadius: const BorderRadius.all(Radius.circular(19)),
-          border: Border.all(color: const Color.fromARGB(255, 232, 232, 232))),
+      decoration: widget.decoration ??
+          BoxDecoration(
+              borderRadius: const BorderRadius.all(Radius.circular(19)),
+              border:
+                  Border.all(color: const Color.fromARGB(255, 232, 232, 232))),
       child: Row(
         mainAxisAlignment: MainAxisAlignment.center,
-        // crossAxisAlignment: CrossAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
         children: [
           const SizedBox(
-            width: 13,
-            height: 13,
+            width: 16,
+            height: 16,
             child: Icon(
               Icons.calendar_month_outlined,
               color: Color.fromARGB(255, 159, 159, 159),
-              size: 13,
+              size: 16,
             ),
           ),
           const SizedBox(
